13c004499d0cd4b22bee4ded43ad9ec2b1e62ac1
KennyWongXX <34906729+KennyWongXX@users.noreply.github.com>
2019-10-15 14:19:43 +0800

    Update label

--- AutoUpdate/AutoUpdate/Form1.cs
+++ AutoUpdate/AutoUpdate/Form1.cs
@@ -23,7 +23,7 @@ namespace AutoUpdate
             {
                 Version myVersion;
                 myVersion = ApplicationDeployment.CurrentDeployment.CurrentVersion;
-                version.Text = string.Concat("Version:", myVersion);
+                version.Text = string.Concat("Testing:", myVersion);
             }
             else
             {
